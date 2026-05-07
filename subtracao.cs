6168c3921aa186d044b4af99ef1828f2cf484292
Ana Leticia <ana.l.felix8@aluno.senai.br>
2026-05-07 14:28:50 -0300

    Add files via upload

---
+++ subtracao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Digite o primeiro número : ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número : ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int resultado = numero1 + numero2;
+        int resultado = numero1 - numero2;
+
+        Console.WriteLine("Resultado :" + resultado);
+        Console.WriteLine("A subtração é :" + resultado);
+    }
+}
